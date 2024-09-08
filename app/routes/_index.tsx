39fd52553fbc10915b7505cc9e6bc7101964b4c7
Hiekki <hiekkii@gmail.com>
2024-09-08 14:39:10 -0500

    update to image size and types

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,27 +13,27 @@ const projects = [
         description:
             'Elenora is a feature rich RuneScape Clan administration discord bot with a wide variety of commands. The creation of Elenora was to provide a reliable, dependable, & helpful tool that would service any sized community.',
         link: '/elenora',
-        image: { url: '/projects/elenora.png', description: 'elenora logo' },
+        image: { url: '/projects/elenora.webp', description: 'elenora logo' },
     },
     {
         title: 'Cora',
         description: 'Cora is a logging bot for Discord. It allows you to log messages, joins, leaves, kicks, bans, and so much more.',
         link: '/cora',
-        image: { url: '/projects/cora.png', description: 'cora logo' },
+        image: { url: '/projects/cora.webp', description: 'cora logo' },
     },
     {
         title: 'Stubby',
         description:
             'A simple Discord ticketing system bot. Allows you to create different categories that allows users to open a ticket based on those categories and have one on one conversations with those individuals.',
         link: '/stubby',
-        image: { url: '/projects/stubby.png', description: 'stubby logo' },
+        image: { url: '/projects/stubby.webp', description: 'stubby logo' },
     },
     {
         title: 'NotiFyre',
         description:
-            'A simple Discord reminder bot. This bot allows you to use simple times like 1h/1hour, 30m/30minutes, or 1d12hr15m, or specific times like 7pm, 7:00pm, or 19:00, or more complex dates and times like "next Friday at 3pm" or "the first Tuesday of next month at noon".',
+            "A simple Discord reminder bot. This bot allows you to use its commands in server the bot isn't in or DMs with friends.",
         link: '/notifyre',
-        image: { url: '/projects/notifyre.png', description: 'notifyre logo' },
+        image: { url: '/projects/notifyre.webp', description: 'notifyre logo' },
     },
 ];
 
